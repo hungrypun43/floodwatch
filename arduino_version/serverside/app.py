dc8a1a90c43d9a8e2b6ca8761e5f6f3ccbd89d99
tamitamboommm!! <pun.hoo.hoo@gmail.com>
2021-03-26 22:38:59 +0700

    Initial flask server setting for add pod (no more netpie because limited flow)

---
+++ arduino_version/serverside/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import pymongo
+import uuid
+
+app = Flask(__name__)
+
+def addpods(data):
+    myclient = pymongo.MongoClient("mongodb+srv://root:password32124@try1.xqhda.mongodb.net/test")
+    mydb = myclient["floodwatch"]
+    mycol = mydb["pods"]
+    newpod = {
+        "podname": data["podname"],
+        "podkey" : str(uuid.uuid4())
+    }
+    x = mycol.insert_one(newpod)
+
+    print(x)
+    return newpod
+
+@app.route("/")
+def greeting():
+    return "hello"
+@app.route("/addpods", methods=["POST"])
+def showreq():
+    data = request.json
+    n = addpods(data)
+    print(n)
+    return "gotit"
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5555', debug=True)
