6beb7174041ce3e340aee7b85762ff2f92d21e9a
tamitamboommm!! <pun.hoo.hoo@gmail.com>
2021-03-27 00:13:38 +0700

    Initial flask authention (no more netpie because limited flow)

--- arduino_version/serverside/app.py
+++ arduino_version/serverside/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import pymongo
 import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'my super secret key'.encode('utf8')
 
 def addpods(data):
     myclient = pymongo.MongoClient("mongodb+srv://root:password32124@try1.xqhda.mongodb.net/test")
@@ -10,19 +12,40 @@ def addpods(data):
     mycol = mydb["pods"]
     newpod = {
         "podname": data["podname"],
-        "podkey" : str(uuid.uuid4())
+        "podkey" : str(uuid.uuid4()),
+        "secretkey" : generate_password_hash(data["podpassword"], method='sha256')
     }
     x = mycol.insert_one(newpod)
 
     print(x)
     return newpod
+def findpod_bykey(key):
+    myclient = pymongo.MongoClient("mongodb+srv://root:password32124@try1.xqhda.mongodb.net/test")
+    mydb = myclient["floodwatch"]
+    mycol = mydb["pods"]
+    u = mycol.find_one({'podkey': key})
+    print(u)
+    return u
 
 @app.route("/")
 def greeting():
     return "hello"
+
+@app.route("/auth")
+def login():
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return "Not found"
+    user = findpod_bykey(auth.username)
+    if not user:
+        return "Not found"
+    if check_password_hash(user["secretkey"], auth.password):
+        return "welcome" + user["podname"]
+    return "fail"
+
 @app.route("/addpods", methods=["POST"])
-def showreq():
-    data = request.json
+def create():
+    data = request.get_json()
     n = addpods(data)
     print(n)
     return "gotit"
