a93218fb29748335a6efc8fed31e41855f0703d1
tamitamboommm!! <pun.hoo.hoo@gmail.com>
2021-03-27 16:06:51 +0700

    add getpodata with key in flask

--- serverside/app.py
+++ serverside/app.py
@@ -6,24 +6,43 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'my super secret key'.encode('utf8')
 
-def addpods(data):
+def connect():
     myclient = pymongo.MongoClient("mongodb+srv://root:password32124@try1.xqhda.mongodb.net/test") #My test server link that will change when you install in another server 
     mydb = myclient["floodwatch"]
+    return mydb
+
+def addpods(data):
+    #myclient = pymongo.MongoClient("mongodb+srv://root:password32124@try1.xqhda.mongodb.net/test") #My test server link that will change when you install in another server 
+    mydb = connect()
     mycol = mydb["pods"]
+    key = str(uuid.uuid4())
     newpod = {
-        "podname": data["podname"],
-        "podkey" : str(uuid.uuid4()),
+        "podkey" : key,
         "secretkey" : generate_password_hash(data["podpassword"], method='sha256')
     }
-    x = mycol.insert_one(newpod)
-
-    print(x)
+    myfeed = mydb["feed"]
+    newfeed = {
+        "podname": data["podname"],
+        "podkey" : key,
+        "podstatus": 0,
+        "height": 0,
+        "setuph": data["sheight"]
+    }
+    mycol.insert_one(newpod)
+    myfeed.insert_one(newfeed)
     return newpod
+
 def findpod_bykey(key):
-    myclient = pymongo.MongoClient("mongodb+srv://root:password32124@try1.xqhda.mongodb.net/test")
-    mydb = myclient["floodwatch"]
+    mydb = connect()
     mycol = mydb["pods"]
-    u = mycol.find_one({'podkey': key})
+    u = mycol.find_one({'podkey': str(key)})
+    print(u)
+    return u
+
+def findpoddata_bykey(key):
+    mydb = connect()
+    mycol = mydb["feed"]
+    u = mycol.find_one({'podkey': str(key)})
     print(u)
     return u
 
@@ -49,5 +68,22 @@ def create():
     n = addpods(data)
     print(n)
     return "gotit"
+
+@app.route("/getpoddata/<keys>", methods=["GET"])
+def show(keys):
+    #return '%s' % keys
+    podauth = findpod_bykey(keys)
+    poddata = findpoddata_bykey(keys)
+    print(podauth)
+    print(poddata)
+    #return "ok"
+    res = {
+        "podname": poddata["podname"],
+        "podkey" : podauth["podkey"],
+        "podseckey": podauth["secretkey"],
+        "podsetupheight": poddata["setuph"]
+    }
+    return res
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5555', debug=True)
